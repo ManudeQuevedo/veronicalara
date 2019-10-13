277cfde42c79670c1c508f62862f57de67b1d99a
Manuel Alejandro Matus de Quevedo <hola@manudequevedo.com>
2019-10-13 15:27:27 -0500

    Configured form

--- src/components/contact/formikConfig.js
+++ src/components/contact/formikConfig.js
@@ -216,6 +216,7 @@ export default () => {
               onVerify={onVerify}
               onLoad={() => onLoad(() => resetForm)}
               size="invisible"
+              theme="light"
             />
             {renderButton(isSubmitting, executing, verified)}
             {/* {errMsg ? <div className="text-primary m-1">{errMsg}</div> : null} */}
