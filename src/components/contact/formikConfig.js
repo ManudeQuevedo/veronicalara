b30b4821a53356b33d06aad755da30bdabf61676
Manu de Quevedo <hola@manudequevedo.com>
2019-10-17 21:04:52 -0500

    Added variables to Netlify and loaded changes in Formik form

--- src/components/contact/formikConfig.js
+++ src/components/contact/formikConfig.js
@@ -1,87 +1,88 @@
-import React, { useRef, useEffect, useState } from "react";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import Reaptcha from "reaptcha";
-import axios from "axios";
-import qs from "qs";
+import React, { useRef, useEffect, useState } from "react"
+import { ErrorMessage, Field, Form, Formik } from "formik"
+import Reaptcha from "reaptcha"
+import axios from "axios"
+import qs from "qs"
+import '../../assets/styles/contact.css'
 
 export default () => {
-  const [errMsg, setErrMsg] = useState("");
-  const [executing, setExecuting] = useState(false);
-  const [formValues, setFormValues] = useState({});
-  const [formReset, setFormReset] = useState({});
-  const [msgSent, setMsgSent] = useState(false);
-  const [rcError, setRcError] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [token, setToken] = useState("");
-  const [verified, setVerified] = useState(false);
+  const [errMsg, setErrMsg] = useState("")
+  const [executing, setExecuting] = useState(false)
+  const [formValues, setFormValues] = useState({})
+  const [formReset, setFormReset] = useState({})
+  const [msgSent, setMsgSent] = useState(false)
+  const [rcError, setRcError] = useState("")
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [token, setToken] = useState("")
+  const [verified, setVerified] = useState(false)
 
-  const rcRef = useRef(null);
+  const rcRef = useRef(null)
 
   useEffect(() => {
     const handleSubmit = async (formValues, token) => {
       const data = {
         ...formValues,
         "g-recaptcha-response": token
-      };
+      }
       const options = {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         data: qs.stringify(data),
         url: "/"
-      };
-      try {
-        await axios(options);
-        setMsgSent(true);
-        formReset();
-      } catch (e) {
-        setErrMsg(e.message);
       }
-    };
-    if (token) {
-      handleSubmit(formValues, token);
+      try {
+        await axios(options)
+        setMsgSent(true)
+        formReset()
+      } catch (e) {
+        setErrMsg(e.message)
+      }
     }
-  }, [formReset, formValues, token]);
+    if (token) {
+      handleSubmit(formValues, token)
+    }
+  }, [formReset, formValues, token])
 
   const onError = () => {
-    console.log("error...");
-    setRcError(true);
-  };
+    console.log("error...")
+    setRcError(true)
+  }
 
   const onExpire = () => {
-    console.log("expired...");
-    console.log("resetting...");
-    resetReCaptcha();
-  };
+    console.log("expired...")
+    console.log("resetting...")
+    resetReCaptcha()
+  }
 
   const onLoad = resetForm => {
-    console.log("loaded...");
-    setFormReset(resetForm);
-  };
+    console.log("loaded...")
+    setFormReset(resetForm)
+  }
 
   const onVerify = token => {
-    console.log("verified...");
-    setToken(token);
-    setVerified(true);
-    setExecuting(false);
-  };
+    console.log("verified...")
+    setToken(token)
+    setVerified(true)
+    setExecuting(false)
+  }
 
   const renderButton = (executing, isSubmitting) => {
     if (errMsg) {
       return (
         <button
-          className="help is-danger"
+          className="failure"
           type="submit"
           disabled
         >
           Mensaje no enviado, por favor inténtalo nuevamente
         </button>
-      );
+      )
     } else if (msgSent) {
       return (
-        <button className="help is-danger" type="submit" disabled>
+        <button className="success" type="submit" disabled>
           Mensaje enviado!
         </button>
-      );
+      )
     } else {
       return (
         <button
@@ -91,27 +92,27 @@ export default () => {
         >
           Enviar mensaje
         </button>
-      );
+      )
     }
-  };
+  }
 
   const resetEverything = resetForm => {
     if (rcError) {
-      setRcError(false);
+      setRcError(false)
     }
     if (resetForm) {
-      setMsgSent(false);
-      setErrMsg(false);
-      resetForm();
+      setMsgSent(false)
+      setErrMsg(false)
+      resetForm()
     }
-    resetReCaptcha();
-  };
+    resetReCaptcha()
+  }
 
   const resetReCaptcha = async () => {
-    console.log("resetting...");
-    await rcRef.current.reset();
-    setVerified(false);
-  };
+    console.log("resetting...")
+    await rcRef.current.reset()
+    setVerified(false)
+  }
 
   return (
     <div>
@@ -125,31 +126,31 @@ export default () => {
           message: ""
         }}
         validate={values => {
-          let errors = {};
+          let errors = {}
           if (!values.username) {
-            errors.username = "Por favor ingresa tu nombre";
+            errors.username = "Por favor ingresa tu nombre"
           }
 
           if (!values.phone) {
-            errors.phone = "Por favor ingresa tu número telefónico";
+            errors.phone = "Por favor ingresa tu número telefónico"
           }
 
           if (!values.email) {
-            errors.email = "Por favor ingresa tu correo electrónico";
+            errors.email = "Por favor ingresa tu correo electrónico"
           } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-            errors.email = "Dirección de correo inválida";
+            errors.email = "Dirección de correo inválida"
           }
 
           if (!values.message) {
-            errors.message = "Por favor ingresa tus dudas o preguntas";
+            errors.message = "Por favor ingresa tus dudas o preguntas"
           }
-          return errors;
+          return errors
         }}
         onSubmit={values => {
-          setIsSubmitting(true);
-          setFormValues({ ...values });
-          setExecuting(true);
-          rcRef.current.execute();
+          setIsSubmitting(true)
+          setFormValues({ ...values })
+          setExecuting(true)
+          rcRef.current.execute()
         }}
       >
         {({ resetForm }) => (
@@ -231,5 +232,5 @@ export default () => {
         )}
       </Formik>
     </div>
-  );
-};
+  )
+}
